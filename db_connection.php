c30ad58b02e3430724c69113ee350090d4944d94
abdellatif guezadi <abdellatifguezadi@gmail.com>
2024-12-15 16:40:28 +0100

    connecter database

---
+++ db_connection.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'itthink';
+$username = 'root'; 
+$password = ''; 
+
+try {
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+?>
+
+
